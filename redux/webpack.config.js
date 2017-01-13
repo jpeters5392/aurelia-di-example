217b8df75bdcee2aef809f6896809b2e0102c29a
Joel Peterson <jpeters5392@gmail.com>
2017-01-13 12:11:33 -0600

    Updated build config

--- redux/webpack.config.js
+++ redux/webpack.config.js
@@ -1,8 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'src');
+var BUILD_DIR = path.resolve(process.cwd(), 'dist');
+var APP_DIR = path.resolve(process.cwd(), 'src');
 
 var config = {
   entry: {
